3b7836a193455ace7bb22d04d3ebaf20d9f3617b
Brandon Swanson <swanyriver@gmail.com>
2016-03-31 14:50:36 -0700

    jinja import

--- hotestprogrammerhumor.py
+++ hotestprogrammerhumor.py
@@ -1,4 +1,18 @@
+import os
+import urllib
+
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
